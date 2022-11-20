8e2703b19dbec671392b140ebc7fdd1edf3f790d
abdullayevazizbek <113992754+abdullayevazizbek@users.noreply.github.com>
2022-11-20 16:56:15 +0000

    first

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function expansion() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+        x.className += " expanded";
+    } else {
+        x.className = "navbar";
+    }
+}
+	
+function search_expand() {
+    var x = document.getElementById("search-responsive");
+    if (x.className === "search-box") {
+        x.className += " expanded";
+    } else {
+        x.className = "search-box";
+    }
+}
